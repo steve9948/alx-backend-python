d321cd0d614370c90f015b2ba1dfa1528bcfc15a
steve9948 <mbuguagichimu@gmail.com>
2024-05-03 14:50:10 +0000

    Basic annotations - add with the README.md file

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" function add ."""
+
+
+def add(a: float, b: float) -> float:
+    """function add that takes a float a,b and returns their sum as a float."""
+    return a + b
