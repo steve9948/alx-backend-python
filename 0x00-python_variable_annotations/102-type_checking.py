cf440a4ce0963bdad61640791cdeb81884c10321
steve9948 <mbuguagichimu@gmail.com>
2024-05-03 17:36:12 +0000

    Type Checking

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Module documentation """
+from typing import List, Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Get the values of the List to validate the function"""
+    zoomed_in: List = [item for item in lst for i in range(factor)]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+zoom_2x = zoom_array(array)
+zoom_3x = zoom_array(array, 3)
