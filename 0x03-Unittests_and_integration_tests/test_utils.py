6757c6e03b7c0f59211d9ccf9186f70533ce61e5
steve9948 <mbuguagichimu@gmail.com>
2024-05-28 13:53:18 +0000

    Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -24,22 +24,20 @@ class TestAccessNestedMap(unittest.TestCase):
 
 
 class TestGetJson(unittest.TestCase):
-    """doc doc doc"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        with patch('utils.requests.get') as mock_get:
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+            mock_get.return_value = mock_response
 
-    @parameterized.expand(
-        [
-            ("http://example.com", {"payload": True}),
-            ("http://holberton.io", {"payload": False}),
-        ]
-    )
-    @patch("requests.get")
-    def test_get_json(
-        self, test_url: str, test_payload: Dict[str, Any], mock_get: Mock
-    ) -> None:
-        """doc doc doc"""
-        mock_get.return_value.json.return_value = test_payload
-        self.assertEqual(get_json(test_url), test_payload)
-        mock_get.assert_called_once_with(test_url)
+            response = get_json(test_url)
+
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(response, test_payload)
 
 
 if __name__ == "__main__":
