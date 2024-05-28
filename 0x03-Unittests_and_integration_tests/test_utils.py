5170307f57b682d7d7f03eac5e0b6acb6c12fe2d
steve9948 <mbuguagichimu@gmail.com>
2024-05-28 13:51:01 +0000

    Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -23,5 +23,24 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(str(context.exception), repr(path[-1]))
 
 
+class TestGetJson(unittest.TestCase):
+    """doc doc doc"""
+
+    @parameterized.expand(
+        [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False}),
+        ]
+    )
+    @patch("requests.get")
+    def test_get_json(
+        self, test_url: str, test_payload: Dict[str, Any], mock_get: Mock
+    ) -> None:
+        """doc doc doc"""
+        mock_get.return_value.json.return_value = test_payload
+        self.assertEqual(get_json(test_url), test_payload)
+        mock_get.assert_called_once_with(test_url)
+
+
 if __name__ == "__main__":
     unittest.main()
