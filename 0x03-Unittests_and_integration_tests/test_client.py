16c9e84c094758fd7627717944315082ac56ff5a
steve9948 <mbuguagichimu@gmail.com>
2024-05-28 12:20:38 +0000

    Trying to debug

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 """Unit tests for utility functions."""
 import unittest
-from parameterized import *
-from utils import *
-from typing import Any, Tuple, Dict
-from unittest.mock import *
+from parameterized import parameterized
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Tests for access_nested_map function"""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
