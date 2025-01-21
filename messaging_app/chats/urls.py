4b6837e45dc51a523ab68956cb8d71b4b72315fd
steve9948 <mbuguagichimu@gmail.com>
2025-01-21 16:13:05 +0300

    Included the use of the DefaultRouter for chats.urls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,10 +2,14 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
+# Instantiate DefaultRouter
 router = DefaultRouter()
+
+# Register viewsets with the router
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
+# Include the router-generated URLs in the urlpatterns
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(router.urls)),  # Include all API routes from the router
 ]
