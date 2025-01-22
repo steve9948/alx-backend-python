ee96b4a7a66a28c520020dbc256ff422ade967cc
steve9948 <mbuguagichimu@gmail.com>
2025-01-22 22:40:49 +0300

    Updated the logging functionality

--- Django-Middleware-0x03/Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/chats/middleware.py
@@ -1,22 +1,19 @@
+# chats/middleware.py
 import logging
 from datetime import datetime
-import os
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        log_dir = os.path.join(os.path.dirname(__file__), 'logs')
-        os.makedirs(log_dir, exist_ok=True)  # Ensure the directory exists
-        log_file = os.path.join(log_dir, 'requests.log')
+        # Configure the logger
         logging.basicConfig(
-            filename=log_file,
+            filename="requests.log",
             level=logging.INFO,
-            format='%(message)s'
+            format="%(message)s",
         )
 
     def __call__(self, request):
         user = request.user if request.user.is_authenticated else "Anonymous"
         log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
         logging.info(log_message)
-        response = self.get_response(request)
-        return response
+        return self.get_response(request)
